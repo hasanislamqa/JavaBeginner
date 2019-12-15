91e01fc82d31f51b59a9ec576de180229e489209
hasanurislam <hasanislamqa@gmail.com>
2019-12-15 10:20:22 -0500

    this is Java for test

---
+++ src/com/task/Task.java
@@ -0,0 +1,12 @@
+package com.task;
+
+public class Task {
+
+
+    public static void main(String[] args) {
+        int banana=30;
+        int apple=20;
+
+        System.out.println(banana+apple);
+    }
+}
